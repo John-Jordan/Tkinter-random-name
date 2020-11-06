fb3ce05b33f4cfaa867af539fdbb99d6d6b51831
John <thejohncjordan@gmail.com>
2020-11-06 15:17:22 -0600

    Added random name printed to console

--- randomname.py
+++ randomname.py
@@ -1,3 +1,4 @@
+import random
 from tkinter import *   
 from tkinter import ttk
 root = Tk()
@@ -5,11 +6,21 @@ root = Tk()
 label = ttk.Label(root, text = "Random Name Selector")
 label.pack()
 label.config(font = ("Courier", 25, 'bold'))
-label.config(foreground = '#64b1bc', background = 'white')
-logo = PhotoImage(file = '/Users/johnjordan/Desktop/Python/Random/neora-LOGO.ppm')
+label.config(foreground = '#64b1bc', background = '#622ab8')
+logo = PhotoImage(file = 'neora-LOGO.ppm')
 new_logo = logo.subsample(7, 7)
 label.config(image = new_logo)
 label.config(compound = 'left')
+
+
+
+
+names = ['Billy Ray', 'Bobby Sue', 'Aunti Em', 'Crocodile Dundee', 'David Hasselhoff']
+
+winner = random.choice(names)
+print(f'The winner is {winner}!!!! Congratulations!')
+
+
 
 
 root.mainloop()

64a9f050854473de353bfc5db7fa6263a06d5b34
John <thejohncjordan@gmail.com>
2020-11-09 14:23:51 -0600

    Added the name entry text box

--- randomname.py
+++ randomname.py
@@ -5,12 +5,17 @@ root = Tk()
 
 label = ttk.Label(root, text = "Random Name Selector")
 label.pack()
-label.config(font = ("Courier", 25, 'bold'))
+label.config(font = ("serif", 25, 'bold'))
 label.config(foreground = '#64b1bc', background = '#622ab8')
 logo = PhotoImage(file = 'neora-LOGO.ppm')
 new_logo = logo.subsample(7, 7)
 label.config(image = new_logo)
 label.config(compound = 'left')
+
+enter_name = ttk.Label(root, text = 'Add name to drawing: ')
+enter_name.pack(side = 'left')
+entry = ttk.Entry(root, width = 24)
+entry.pack(side = 'left')
 
 
 
